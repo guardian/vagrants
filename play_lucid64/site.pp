fefad11230849532ba150aa46c930cda194a2d75
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-06-22 14:01:45 +0100

    Require the nginx package before messing with its configuration.

--- play_lucid64/site.pp
+++ play_lucid64/site.pp
@@ -5,14 +5,17 @@ node default {
   file {
     "/etc/nginx/conf.d/default.conf":
       ensure => absent,
+      require => Package[nginx],
       notify => Service[nginx];
 
     "/etc/nginx/conf.d/example_ssl.conf":
       ensure => absent,
+      require => Package[nginx],
       notify => Service[nginx];
 
     "/etc/nginx/conf.d/frontend.conf":
       source => 'puppet:///files/etc/nginx/conf.d/frontend.conf',
+      require => Package[nginx],
       notify => Service[nginx];
   }
 }
