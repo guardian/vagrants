251954f45facb14ef67e1ce7f69c0141de7cdfbc
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-06-22 14:07:21 +0100

    Add play_extras_precise64 for Ubuntu 12.04

---
+++ play_extras_precise64/site.pp
@@ -0,0 +1,4 @@
+node default {
+  include elasticsearch
+  include mongodb
+}
--- puppet/modules/mongodb/manifests/init.pp
+++ puppet/modules/mongodb/manifests/init.pp
@@ -7,19 +7,19 @@ class mongodb {
   }
 
   file {
-    '/etc/apt/sources.list.d': ensure => directory;
+    "/etc/apt/sources.list.d": ensure => directory;
 
-    '/etc/apt/sources.list.d/10gen-lucid.list':
-      source => 'puppet:///modules/mongodb/etc/apt/sources.list.d/10gen-lucid.list';
+    "/etc/apt/sources.list.d/10gen-upstart.list":
+      source => "puppet:///modules/mongodb/etc/apt/sources.list.d/10gen-upstart.list";
 
-    '/etc/apt/trusted.gpg.d/10gen.gpg':
-      source => 'puppet:///modules/mongodb/etc/apt/trusted.gpg.d/10gen.gpg';
+    "/etc/apt/trusted.gpg.d/10gen.gpg":
+      source => "puppet:///modules/mongodb/etc/apt/trusted.gpg.d/10gen.gpg";
   }
 
   exec {
     apt-add-10gen-key:
-      command => '/usr/bin/apt-get update',
-      subscribe => File['/etc/apt/trusted.gpg.d/10gen.gpg'],
+      command => "/usr/bin/apt-get update",
+      subscribe => File["/etc/apt/trusted.gpg.d/10gen.gpg"],
       refreshonly => true;
   }
 
@@ -27,14 +27,14 @@ class mongodb {
     mongodb-10gen:
       ensure => installed,
       require => [
-        File['/etc/apt/sources.list.d/10gen-lucid.list'],
-        Exec['apt-add-10gen-key']
+        File["/etc/apt/sources.list.d/10gen-upstart.list"],
+        Exec["apt-add-10gen-key"]
       ];
   }
 
   service {
     mongodb:
       ensure => running,
-      require => Package['mongodb-10gen'];
+      require => Package["mongodb-10gen"];
   }
 }
