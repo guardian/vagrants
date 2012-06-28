4f11736eb667a95eebf3863f405e1482cd25a2a3
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-06-28 18:15:57 +0100

    Zookeeper puppet module

---
+++ puppet/modules/zookeeper/manifests/init.pp
@@ -0,0 +1,34 @@
+class zookeeper {
+
+  package {
+    zookeeper: ensure => installed;
+  }
+
+  File {
+    owner => root,
+    group => root,
+    mode => 644
+  }
+
+  file {
+    "/usr/share/zookeeper/conf": ensure => directory;
+
+    "/usr/share/zookeeper/conf/zoo.cfg":
+      require => File["/usr/share/zookeeper/conf"],
+      source => "puppet:///modules/zookeeper/usr/share/zookeeper/conf/zoo.cfg";
+  }
+
+  service {
+    zookeeper:
+      provider => "init",
+      hasstatus => true,
+      start => "/usr/share/zookeeper/bin/zkServer.sh start",
+      status => "/usr/share/zookeeper/bin/zkServer.sh status",
+      stop => "/usr/share/zookeeper/bin/zkServer.sh stop",
+      ensure => running;
+  }
+
+  Package["zookeeper"] ->
+    File["/usr/share/zookeeper/conf", "/usr/share/zookeeper/conf/zoo.cfg"] ->
+    Service["zookeeper"]
+}
